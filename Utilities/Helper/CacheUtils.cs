880348bfb4105032427e61b0a75927eb76ded16c
ProbisMis <buraksmeric@gmail.com>
2020-10-17 17:52:01 +0300

    log config added

---
+++ Utilities/Helper/CacheUtils.cs
@@ -0,0 +1,85 @@
+﻿using GoodJobGames.Models.CacheModel;
+using GoodJobGames.Models.Responses;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace GoodJobGames.Utilities.Helper
+{
+    public static class CacheUtils
+    {
+        public static HashEntry[] generateHashEntry(UserResponse model)
+        {
+            HashEntry[] hashEntries = new HashEntry[1];
+            hashEntries.Append(new HashEntry("Username", model.Username));
+
+            return hashEntries;
+        }
+
+        public static List<string> generateStringFromHashEntry(HashEntry[] model)
+        {
+            List<string> response = new List<string>();
+            foreach (var item in model)
+            {
+                response.Add(item.ToString());
+            }
+            return response;
+        }
+
+        public static List<LeaderboardCacheModel> ToListLeaderboardCacheModel(RedisValue[] values)
+        {
+            List<LeaderboardCacheModel> response = new List<LeaderboardCacheModel>();
+            foreach (var item in values)
+            {
+                response.Add(JsonConvert.DeserializeObject<LeaderboardCacheModel>(item.ToString()));
+            }
+            return response;
+        }
+
+        public static  Dictionary<string, string> generateMapFromHashEntry(HashEntry[] model)
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            foreach (var item in model)
+            {
+                keyValuePairs.Add(item.Name, item.Value);
+            }
+            return keyValuePairs;
+        }
+
+        public static HashEntry[] ToHashEntries(this object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+            return properties
+                .Where(x => x.GetValue(obj) != null) // <-- PREVENT NullReferenceException
+                .Select
+                (
+                      property =>
+                      {
+                          object propertyValue = property.GetValue(obj);
+                          string hashValue;
+
+                          // This will detect if given property value is 
+                          // enumerable, which is a good reason to serialize it
+                          // as JSON!
+                          if (propertyValue is IEnumerable<object>)
+                          {
+                              // So you use JSON.NET to serialize the property
+                              // value as JSON
+                              hashValue = JsonConvert.SerializeObject(propertyValue);
+                          }
+                          else
+                          {
+                              hashValue = propertyValue.ToString();
+                          }
+
+                          return new HashEntry(property.Name, hashValue);
+                      }
+                )
+                .ToArray();
+        }
+    }
+}
