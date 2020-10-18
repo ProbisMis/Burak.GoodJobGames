eb64608e1da3ab74817955e8c73e865e4877df2f
ProbisMis <buraksmeric@gmail.com>
2020-10-18 15:55:11 +0300

    model adjustments

---
+++ Models/CacheModel/CacheKeyModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodJobGames.Models.CacheModel
+{
+    public class CacheKeyModel
+    {
+        public string Key { get; set; }
+    }
+}
--- Models/CustomExceptions/ServiceAdaptorException.cs
+++ Models/CustomExceptions/ServiceAdaptorException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,16 +8,19 @@ namespace GoodJobGames.Models.CustomExceptions
 {
     public class ServiceAdaptorException
     {
+        [JsonIgnore]
         /// <summary>
         /// Error code for service owners. We do not need any error code for response, but
         /// it is easy to determine where the error occures when there is a code for it.
         /// </summary>
         public string ErrorCode { get; set; }
+        [JsonIgnore]
         /// <summary>
         /// Error message for the return value. If UserFriendly value is true then we print
         /// this message to user else we print a default error message.
         /// </summary>
         public string Message { get; set; }
+        [JsonIgnore]
         /// <summary>
         /// Determines if the error message will print to screen or not.
         /// </summary>
